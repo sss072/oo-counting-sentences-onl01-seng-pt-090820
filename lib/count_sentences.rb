e18f6c93e3024be8b79f21385041d4af7f70c9aa
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-10-01 21:50:45 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
+    self.end_with?(".")
     
   end
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    array = self.split(/[.?!]/).delete_if {|element| element.length < 2}
+    array.length 
+     
 
   end
 end
